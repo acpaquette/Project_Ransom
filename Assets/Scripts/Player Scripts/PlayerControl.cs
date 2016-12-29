b0f0a4015d736f166b02ac04873a0fa30c20a1f1
Ribuster <gamdevmail@gmail.com>
2016-12-29 02:17:53 -0700

    Initial commit of code base.

---
+++ Assets/Scripts/Player Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	private PlayerMovement movement;
+	private Rigidbody2D rb;
+	public int speed;
+	public bool canMove;
+
+	// Use this for initialization
+	void Start () {
+		movement = GetComponent<PlayerMovement>();
+		rb = GetComponent<Rigidbody2D>();
+		canMove = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Jump")) {
+			canMove = false;
+			Debug.Log("Hahaha");
+		}
+		else {
+			canMove = true;
+		}
+	}
+
+	void FixedUpdate () {
+		if (Input.GetButton("Right") && rb.velocity.x <= speed && canMove) {
+			movement.MoveRight();
+		}
+		if (Input.GetButton("Left") && rb.velocity.x >= -speed && canMove) {
+			movement.MoveLeft();
+		}
+		if (Input.GetButton("Up") && rb.velocity.y <= speed && canMove) {
+			movement.MoveUp();
+	  }
+	  if (Input.GetButton("Down") && rb.velocity.y >= -speed && canMove) {
+		  movement.MoveDown();
+		}
+  }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Handles the physics of player movement
+public class PlayerMovement : MonoBehaviour, IMovement {
+	public float xStart;
+	public float yStart;
+
+	public float xSpeed;
+	public float ySpeed;
+	private Rigidbody2D rb;
+	private Vector2 playerVelocity;
+
+	private double xPrevious;
+	private double yPrevious;
+	private float magnitude;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		name = rb.name;
+		GetComponent<Rigidbody2D>().position = new Vector2(xStart, yStart);
+	}
+
+	// Update is called every significant frame
+  public void Update () {
+		xPrevious = rb.position.x;
+		yPrevious = rb.position.y;
+	}
+
+	public void FixedUpdate () {
+	}
+
+	// Moves the player up by the ySpeed as a normalized physics vector
+	public void MoveUp () {
+		Vector2 vector = rb.velocity;
+		vector.y += ySpeed;
+	  magnitude = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+		vector = ySpeed * (vector/magnitude);
+		rb.velocity = vector;
+	}
+
+	// Moves the player down by the negation of the ySpeed as a normalized physics vector
+	public void MoveDown () {
+		Vector2 vector = rb.velocity;
+		vector.y -= ySpeed;
+		magnitude = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+		vector = ySpeed * (vector/magnitude);
+		rb.velocity = vector;
+	}
+
+	// Moves the player to the right by the xSpeed as a normalized physics vector
+	public void MoveRight () {
+		Vector2 vector = rb.velocity;
+		vector.x += xSpeed;
+		magnitude = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+		vector = xSpeed * (vector/magnitude);
+		rb.velocity = vector;
+	}
+
+	// Moves the player to the left by the negation of the xSpeed as a normalized physics vector
+	public void MoveLeft () {
+		Vector2 vector = rb.velocity;
+		vector.x -= xSpeed;
+		magnitude = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+		vector = xSpeed * (vector/magnitude);
+		rb.velocity = vector;
+	}
+
+	// Will immidiatly stop the players movement
+	// Generally don't like this as a way to stop the player
+	public void Stop () {
+		rb.velocity = new Vector2(0, 0);
+	}
+}
